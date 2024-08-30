29ae31d5aaddab19fc8fd3bb854b86d42bff376e
Md Jahid Hasan <119929408+jahid-facts@users.noreply.github.com>
2024-08-30 17:48:56 +0600

    updated

--- app/Controllers/UserController.php
+++ app/Controllers/UserController.php
@@ -4,13 +4,33 @@ namespace App\Controllers;
 use App\Models\User;
 
 class UserController{
+    // view
     public function index() {
         $users = User::getAllUsers();
         view('profile', ['users' => $users]);
     }
 
+    // show user's profile by ID
     public function show($id) {
         $user = User::getUserById($id);
         view('users.show', ['user' => $user]);
+    }
+
+    // edit user's profile by ID
+    public function edit($id) {
+        $user = User::getUserById($id);
+        view('users.edit', ['user' => $user]);
+    }
+
+    // update user's profile by ID
+    public function update($id) {
+        $user = User::getUserById($id);
+        view('users.update', ['user' => $user]);
+    }
+
+    // delete user's profile by ID
+    public function delete($id) {
+        $user = User::getUserById($id);
+        view('users.delete', ['user' => $user]);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -8,6 +8,10 @@ use App\Controllers\UserController;
 use App\Middleware\AuthMiddleware;
 
 Route::get('/', [FrontendController::class, "index"], [AuthMiddleware::class]);
+Route::get('/dashboard', [DashboardController::class, "index"]);
 Route::get('/profile', [UserController::class, "index"], [AuthMiddleware::class]);
 Route::get('/profile/{id}', [UserController::class, "show"], [AuthMiddleware::class]);
-Route::get('/dashboard', [DashboardController::class, "index"]);
+Route::post('/profile', [UserController::class, "store"], [AuthMiddleware::class]);
+Route::put('/profile/{id}', [UserController::class, "update"], [AuthMiddleware::class]);
+Route::patch('/profile/{id}', [UserController::class, "edit"], [AuthMiddleware::class]);
+Route::delete('/profile/{id}', [UserController::class, "delete"], [AuthMiddleware::class]);
